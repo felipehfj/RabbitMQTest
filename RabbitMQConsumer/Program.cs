ddf8e2f1cd38b446d166a2fa2f81de03ef10b64f
Elias Rodrigues de Sousa <e.sousa@outlook.com.br>
2022-11-18 09:17:09 -0300

    Adicionar arquivos de projeto.

---
+++ RabbitMQConsumer/Program.cs
@@ -0,0 +1,40 @@
+﻿using RabbitMQ.Client.Events;
+using RabbitMQ.Client;
+using System.Text;
+
+var factory = new ConnectionFactory() { HostName = "192.168.1.13", Port = 5672 };
+using (var connection = factory.CreateConnection())
+
+using (var channel = connection.CreateModel())
+{
+    channel.QueueDeclare(queue: "hello",
+                         durable: false,
+                         exclusive: false,
+                         autoDelete: false,
+                         arguments: null);
+
+    var consumer = new EventingBasicConsumer(channel);
+    consumer.Received += (model, ea) =>
+    {
+        var body = ea.Body.ToArray();
+        var message = Encoding.UTF8.GetString(body);
+        Console.WriteLine(" [x] Received {0}", message);
+        Task.Delay(1000).Wait();
+    };
+
+    channel.BasicConsume(queue: "hello",
+                         autoAck: true,
+                         consumer: consumer);
+
+    //while (true)
+    //{
+    //    var data = channel.BasicGet("hello", true);
+    //    var message = Encoding.UTF8.GetString(data.Body.ToArray());
+    //    Console.WriteLine(" [x] Received {0}", message);
+    //    Task.Delay(1000).Wait();
+    //}
+
+
+    Console.WriteLine(" Press [enter] to exit.");
+    Console.ReadLine();
+}
---
+++ RabbitMQProducer/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+
+var factory = new ConnectionFactory() { HostName = "192.168.1.13", Port = 5672 };
+using (var connection = factory.CreateConnection())
+
+using (var channel = connection.CreateModel())
+{
+
+    foreach (var item in Enumerable.Range(1, 1000))
+    {
+        channel.QueueDeclare(queue: "hello",
+                        durable: false,
+                        exclusive: false,
+                        autoDelete: false,
+                        arguments: null);
+
+        string message = $"{Guid.NewGuid().ToString().Replace("-","")}";
+        var body = Encoding.UTF8.GetBytes(message);
+
+        channel.BasicPublish(exchange: "",
+                             routingKey: "hello",
+                             basicProperties: null,
+                             body: body);
+        Console.WriteLine($" [{item}] Sent {message}");
+    }
+   
+}
